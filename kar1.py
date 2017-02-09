57b5c0285e7d117199304f9474efb89ff5ca089d
KaranMatlani <karanmatlani6@gmail.com>
2017-02-09 15:38:26 +0530

    Q1

---
+++ kar1.py
@@ -0,0 +1,5 @@
+le=int(input("Enter the length of the rectangle :"))
+bre=int(input("Enter the length of the rectangle :"))
+A=le*bre
+P=2*(le+bre)
+print(("The area is %d and the perimeter is %d") % (A,P))
